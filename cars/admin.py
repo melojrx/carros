5fd1ca3dcef4e6fc04a52e8f41545d75e9428fe2
Melo Jr <jrmeloafrf@gmal.com>
2023-08-17 15:00:16 -0300

    first commit

---
+++ cars/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from cars.models import Car, Brand
+
+# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand','model','factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand')
+
+
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)
---
+++ readme.md
@@ -0,0 +1,1 @@
+Sistema para revenda de carros feito em Django para estudos. 
